a7f704cf4a3212b8ba19f42994731325aa22cdf0
Margarita <margaritagarrido95@hotmail.com>
2024-02-19 16:20:00 +0100

    puntoycoma

--- src/main.ts
+++ src/main.ts
@@ -1,46 +1,42 @@
 interface Grupos {
-    nombre: string;
-    año: number;
-    activo: boolean;
-    genero; string;
+  nombre: string;
+  año: number;
+  activo: boolean;
+  genero: string;
 }
 
-const grupoA : Grupos {
-    nombre:"The Beatles";
-    año:1960
-    activo: true
-    Genero:"Pop Rock"
-}
+const grupoA: Grupos = {
+  nombre: "The Beatles",
+  año: 1960,
+  activo: true,
+  genero: "Pop Rock",
+};
 
-const grupoB : Grupos {
-    nombre:"Queen";
-    año: 1970;
-    activo: false;
-    genero:"Rock";
-}
+const grupoB: Grupos = {
+  nombre: "Queen",
+  año: 1970,
+  activo: false,
+  genero: "Rock",
+};
 
-const grupoC : Grupos {
-    nombre:"AC DC";
-    año: 1973;
-    activo: true;
-    genero:"Hard Rock";
-}
+const grupoC: Grupos = {
+  nombre: "AC DC",
+  año: 1973,
+  activo: true,
+  genero: "Hard Rock",
+};
 
-const grupoD : Grupos {
-    nombre:"Ludwig van Beethoven";
-    año:1770;
-    activo: false;
-    genero: "Clásica"
-}
+const grupoD: Grupos = {
+  nombre: "Ludwig van Beethoven",
+  año: 1770,
+  activo: false,
+  genero: "Clásica",
+};
 
-const grupoE : Grupos {
-    nombre:"The Rolling Stones";
-    año:1962;
-    activo: true;
-    genero:"Rock";
-}
-console.log(grupoB);
-
-
-
-
+const grupoE: Grupos = {
+  nombre: "The Rolling Stones",
+  año: 1962,
+  activo: true,
+  genero: "Rock",
+};
+console.log(grupoE.año);
